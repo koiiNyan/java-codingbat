58850bf6c95e716301c4420c96eb2df31120e570
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-10-08 21:14:21 +0300

    Create countXX.java

---
+++ countXX.java
@@ -0,0 +1,15 @@
+/*
+Count the number of "xx" in the given string. We'll say that overlapping is allowed,
+so "xxx" contains 2 "xx".
+*/
+int countXX(String str) {
+  int counter = 0;
+  for (int i=0; i<str.length()-1; i++)
+  {
+    if (str.substring(i,i+2).equals("xx"))
+    {
+      counter +=1;
+    }
+  }
+  return counter;
+}
