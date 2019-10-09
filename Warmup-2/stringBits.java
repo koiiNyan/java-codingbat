519f584155b2f339dd5aa343df4a677b690e5436
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-10-09 09:28:53 +0300

    Update stringBits.java

--- Warmup-2/stringBits.java
+++ Warmup-2/stringBits.java
@@ -1,7 +1,7 @@
 /*
 Given a string, return a new string made of every other char starting with the first,
 so "Hello" yields "Hlo".
-/*
+*/
 public String stringBits(String str) {
   String desired = new String();
   for (int i=0; i<str.length(); i+=2)
