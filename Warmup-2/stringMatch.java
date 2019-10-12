425408b6599f510b1cf635cbab5fab40307b5e34
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-10-12 18:20:58 +0300

    Create stringMatch.java

---
+++ Warmup-2/stringMatch.java
@@ -0,0 +1,14 @@
+/*
+Given 2 strings, a and b, return the number of the positions
+where they contain the same length 2 substring.
+So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings
+appear in the same place in both strings.
+*/
+public int stringMatch(String a, String b) {
+  int counter = 0;
+  for(int i=0; i<= Math.min(a.length(), b.length())-2; i++)
+  {
+    if(a.substring(i, i+2).equals(b.substring(i, i+2))) counter++;
+  }
+  return counter;
+}
