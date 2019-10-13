da1e4a9256c21c665e7bbc9b53fde583c1b73c2e
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-10-13 13:01:55 +0300

    Create has271.java

---
+++ Warmup-2/has271.java
@@ -0,0 +1,17 @@
+/*
+Given an array of ints, return true if it contains a 2, 7, 1 pattern:
+a value, followed by the value plus 5, followed by the value minus 1.
+Additionally the 271 counts even if the "1" differs by 2 or less from the correct value.
+*/
+public boolean has271(int[] nums) {
+  for (int i=0; i<=nums.length-2;i++)
+  {
+    if(nums[i+1]==nums[i]+5)
+    {
+    // abs to check if the absolute value of 3rd num differs from the correct value by 2 or less  
+      if(Math.abs(nums[i+2] - (nums[i]-1)) <=2)
+      return true;
+    }
+  }
+  return false;
+}
