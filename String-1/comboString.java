cfeac07d816028c26d9e6131abb602e6eaced58b
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-10-13 16:10:45 +0300

    Create comboString.java

---
+++ String-1/comboString.java
@@ -0,0 +1,20 @@
+/*
+Given 2 strings, a and b, return a string of the form short+long+short,
+with the shorter string on the outside and the longer string on the inside.
+The strings will not be the same length, but they may be empty (length 0).
+*/
+public String comboString(String a, String b) {
+  String max = new String();
+  String min = new String();
+  if (a.length() > b.length()) 
+  {
+    max = a;
+    min = b;
+  }
+  else
+  {
+    max = b;
+    min = a;
+  }
+  return min + max + min;
+}
