bfad8a74539c2a01baf706df1594d8d96e65eab2
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-10-17 09:15:53 +0300

    Create  lastChars.java

---
+++ String-1/lastChars.java
@@ -0,0 +1,24 @@
+/*
+Given 2 strings, a and b, return a new string made of the first char
+of a and the last char of b, so "yo" and "java" yields "ya".
+If either string is length 0, use '@' for its missing char.
+*/
+public String lastChars(String a, String b) {
+  String aFirst = new String();
+  String bLast = new String();
+  if (a.length() == 0)
+  {
+    aFirst = "@";
+  } else 
+  {
+    aFirst = a.substring(0,1);
+  }
+  if (b.length() == 0)
+  {
+    bLast = "@";
+  } else 
+  {
+    bLast = b.substring(b.length()-1);
+  }
+  return aFirst + bLast;
+}
