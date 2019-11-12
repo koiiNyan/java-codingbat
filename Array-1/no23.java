24013edb9845cf6a1e2abeb3a12c25f18413ec34
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-11-12 15:30:55 +0300

    Create no23.java

---
+++ Array-1/no23.java
@@ -0,0 +1,11 @@
+/*
+Given an int array length 2, return true if it does not contain a 2 or 3.
+*/
+public boolean no23(int[] nums) {
+  for (int i : nums)
+  {
+    if (i==2 || i==3) return false;
+  }
+  return true;
+}
+
