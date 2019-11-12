ab58acd6a1ff603a03ac5b547e6be7fbace817a4
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-11-12 15:22:52 +0300

    Create has23.java

---
+++ Array-1/has23.java
@@ -0,0 +1,10 @@
+/*
+Given an int array length 2, return true if it contains a 2 or a 3.
+*/
+public boolean has23(int[] nums) {
+  for (int i : nums)
+  {
+    if (i==2 || i==3) return true;
+  }
+  return false;
+}
