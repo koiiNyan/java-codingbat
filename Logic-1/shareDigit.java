73c0c9e3e0d413ba0631415248783bd9124ec7de
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-11-23 11:53:03 +0300

    Create shareDigit.java

---
+++ Logic-1/shareDigit.java
@@ -0,0 +1,8 @@
+/*
+Given two ints, each in the range 10..99, return true
+if there is a digit that appears in both numbers, such as the 2 in 12 and 23. 
+*/
+public boolean shareDigit(int a, int b) {
+  return(a/10 == b/10 || a%10 == b%10 ||
+        a/10 == b % 10 || a%10 == b/10);
+}
