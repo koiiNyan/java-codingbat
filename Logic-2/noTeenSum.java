334b4bc88d4b94a655cacbdc4e068b066088c73f
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-11-23 14:03:53 +0300

    Create noTeenSum.java

---
+++ Logic-2/noTeenSum.java
@@ -0,0 +1,21 @@
+/*
+Given 3 int values, a b c, return their sum.
+However, if any of the values is a teen -- in the range 13..19
+inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. 
+Write a separate helper "public int fixTeen(int n) {"that takes in an int value and returns
+that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+Define the helper below and at the same indent level as the main noTeenSum().
+*/
+public int noTeenSum(int a, int b, int c) {
+  int aNorm = fixTeen(a);
+  int bNorm = fixTeen(b);
+  int cNorm = fixTeen(c);
+  return aNorm+bNorm+cNorm;
+}
+
+public int fixTeen(int num) {
+  int fixed = (num>=12 && num <=19)? 0 : num;
+  if(num==15) fixed=15;
+  if(num==16) fixed=16;
+  return fixed;
+}
