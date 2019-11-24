d5a5b1802be4bcabe666acfa9e2ae84380c53208
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-11-24 14:06:53 +0300

    Create catDog.java

---
+++ String-2/catDog.java
@@ -0,0 +1,13 @@
+/*
+Return true if the string "cat" and "dog" appear the same number of times in the given string.
+*/
+public boolean catDog(String str) {
+  int catCount = 0;
+  int dogCount = 0;
+  for (int i=0; i < str.length()-2; i++)
+  {
+    if(str.substring(i,i+3).equals("cat")) catCount++;
+    else if(str.substring(i,i+3).equals("dog")) dogCount++;
+  }
+  return(catCount == dogCount);
+}
