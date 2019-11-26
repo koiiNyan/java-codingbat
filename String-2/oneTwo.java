3688df25c205e6c82fc1bd212df2e5758a16521f
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2019-11-26 10:32:50 +0300

    Create oneTwo.java

---
+++ String-2/oneTwo.java
@@ -0,0 +1,14 @@
+/*
+Given a string, compute a new string by moving the first char to come after the next two chars,
+so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields
+"bcaefd". Ignore any group of fewer than 3 chars at the end.
+*/
+public String oneTwo(String str) {
+  String desired = new String();
+  for(int i=0;i<str.length()-2;i+=3)
+  {
+    desired+= str.substring(i+1,i+3);
+    desired += str.substring(i,i+1);
+  }
+  return desired;
+}
