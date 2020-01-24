1a0dfd9c49d37ecec6728ea528a0378c5f950679
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2020-01-24 12:57:41 +0300

    Create changeXY.java

---
+++ Recursion-1/changeXY.java
@@ -0,0 +1,10 @@
+/*
+Given a string, compute recursively (no loops) a new string
+where all the lowercase 'x' chars have been changed to 'y' chars.
+*/
+public String changeXY(String str) {
+  if (str.equals("")) return "";
+  if (str.charAt(0) == 'x') 
+    return 'y' + changeXY(str.substring(1));
+  return str.charAt(0) + changeXY(str.substring(1));
+}
