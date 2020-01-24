5829fb27f823cc9e9a5b3bb7d44e471269e9d16d
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2020-01-24 15:17:53 +0300

    Create allStar.java

---
+++ Recursion-1/allStar.java
@@ -0,0 +1,7 @@
+/*
+Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+*/
+public String allStar(String str) {
+  if (str.equals("")  || str.length() == 1) return str;
+  return str.charAt(0) + "*" + allStar(str.substring(1));
+}
