3389235d7bfbae5773b3d93222d4205b02395c84
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2020-02-19 14:37:01 +0300

    Create scoreUp.java

---
+++ AP-1/scoreUp.java
@@ -0,0 +1,18 @@
+/*
+The "key" array is an array containing the correct answers to an exam,
+like {"a", "a", "b", "b"}. the "answers" array contains a student's answers,
+with "?" representing a question left blank. The two arrays are not empty
+and are the same length. Return the score for this array of answers, giving +4
+for each correct answer, -1 for each incorrect answer, and +0 for each blank answer.
+*/
+public int scoreUp(String[] key, String[] answers) {
+  int score = 0;
+  for (int i=0; i < key.length; i++)
+  {
+    if(key[i] == answers[i])
+      score += 4;
+    else if(key[i] != answers[i] && answers[i] != "?") 
+      score--;
+  }
+  return score;
+}
