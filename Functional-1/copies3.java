45f998d3852f46869ec18ddbab284077720ab08d
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2020-02-25 12:38:02 +0300

    Create copies3.java

---
+++ Functional-1/copies3.java
@@ -0,0 +1,7 @@
+/*
+Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+*/
+public List<String> copies3(List<String> strings) {
+  strings.replaceAll(n -> n+n+n);
+  return strings;
+}
