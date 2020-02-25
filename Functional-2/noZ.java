3ff7131bbe34fd7b20bc28b3f7b5a78c5bd19ed5
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2020-02-25 12:54:41 +0300

    Create noZ.java

---
+++ Functional-2/noZ.java
@@ -0,0 +1,9 @@
+/*
+Given a list of strings, return a list of the strings,
+omitting any string that contains a "z".
+(Note: the str.contains(x) method returns a boolean)
+*/
+public List<String> noZ(List<String> strings) {
+  strings.removeIf(n-> n.contains("z"));
+  return strings;
+}
