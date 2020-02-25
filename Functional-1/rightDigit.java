aae59edcea99348a75a52c8c9ac764bcc48e6142
Anastasia <37482238+ovinnikova@users.noreply.github.com>
2020-02-25 12:43:59 +0300

    Create rightDigit.java

---
+++ Functional-1/rightDigit.java
@@ -0,0 +1,7 @@
+/*
+Given a list of non-negative integers, return an integer list of the rightmost digits. (Note: use %)
+*/
+public List<Integer> rightDigit(List<Integer> nums) {
+  nums.replaceAll(n-> n%10);
+  return nums;
+}
